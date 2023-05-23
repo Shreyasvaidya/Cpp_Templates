a4fd8db41c7aacce28e0e03ec4483bf78d45381b
Shreyas Vaidya <95679215+Shreyasvaidya@users.noreply.github.com>
2023-05-23 14:08:33 +0530

    Added binary exponentiation and modular inverse

--- modular_arithmetic.cpp
+++ modular_arithmetic.cpp
@@ -1,7 +1,37 @@
 #include<iostream>
-long long int addMod(long long x,long long y,int mod){
-	return ((x)%mod + (y)%mod)%mod;
+
+const int mod = 1000000007;
+long long modularAdd(long long a, long long b,int modulus = mod){
+	return (a%modulus+ b%modulus)%modulus; 
 }
-long long int multMod(long long x,long long y,int mod){
-	return( (x%mod) * (y%mod))%mod;
+long long modularMult(long long a, long long b,int modulus = mod){
+	return ((a%modulus)* (b%modulus))%modulus;
+}
+void modularPrecomputeFactorial(long long upperLimit,long long *arr){
+	arr[0]=1;
+	arr[1] = 1;
+	for(int i=2;i<=upperLimit;i++){
+		arr[i] = modularMult(arr[i-1],i);
+	}
+}
+long long binExpo(long long n,long long pow, int modulus = mod){
+	long long ans=1;
+	long long toBeMultiplied = n;
+	while(pow>0){
+		if(pow%2){
+			ans = modularMult(ans,toBeMultiplied);
+		}
+		pow = pow>>1;
+		toBeMultiplied =modularMult(toBeMultiplied,toBeMultiplied);
+	}
+	return ans;
+}
+
+long long modInv(long long n){
+	return binExpo(n,mod-2);
+}
+long long modular_nCr(long long n,long long r,long long *factorialArr,int modulus = mod){
+	long long Nr = factorialArr[n]%mod;
+	long long Dr = modularMult(factorialArr[r],factorialArr[n-r]);
+	return modularMult(Nr,modInv(Dr));
 }
